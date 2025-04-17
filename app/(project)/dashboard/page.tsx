2e7d2137d4966fecb169b8946e7a56ed22f2c80a
Lbnkz <lucasbaixonunes@gmail.com>
2025-04-17 01:59:55 -0300

    Second Day

---
+++ app/(project)/dashboard/page.tsx
@@ -0,0 +1,27 @@
+import { handleAuth } from "@/app/actions/handle-auth";
+import { auth } from "@/app/lib/auth"
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+    const session = await auth();
+
+    if (!session) {
+        redirect("/login");
+    }
+
+    return (
+        <div className="flex flex-col gap-10 items-center justify-center min-h-screen p-24">
+            <h1 className="text-4xl font-bold">Protected Dashboard</h1>
+            <p>{session?.user?.email ? session.user.email : "User not logged in!"}</p>
+            {
+                session?.user?.email && (
+                    <form
+                        action={handleAuth}
+                    >
+                        <button type="submit" className="bg-blue-500 text-white p-2 rounded cursor-pointer">Logout</button>
+                    </form>
+                )
+            }
+        </div>
+    )
+}
--- app/(project)/login/page.tsx
+++ app/(project)/login/page.tsx
@@ -1,7 +1,14 @@
+import { handleAuth } from "@/app/actions/handle-auth";
+
 export default function Login() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-24">
+      <h1 className="text-4xl font-bold mb-10">Login</h1>
+      <form
+        action={handleAuth}
+      >
+        <button type="submit" className="bg-blue-500 text-white p-2 rounded cursor-pointer">Signin with Google</button>
+      </form>
+    </div>
+  )
 }
